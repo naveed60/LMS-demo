d1c84fbe0aff0794d1cf2f460ea57c016a6ba54d
naveed60 <advmiannaveedali@gmail.com>
2024-03-06 11:24:00 +0500

    schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_31_120519) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_06_061837) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -91,6 +91,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_31_120519) do
 
   create_table "grades", force: :cascade do |t|
     t.string "letter"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
